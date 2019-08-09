1c3b725e3d15670cb9f8cd1415503f59717f628f
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-08-09 14:39:26 -0400

    Added version

--- SuperStart/Properties/AssemblyInfo.cs
+++ SuperStart/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SuperStart")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SuperStart")]
+[assembly: AssemblyTitle("Super Start - Computer Lockdown & Re-launcher")]
+[assembly: AssemblyDescription("Re-launches programs & locks computer down.")]
+[assembly: AssemblyConfiguration("Dylan Bickerstaff")]
+[assembly: AssemblyCompany("Dylan Bickerstaff")]
+[assembly: AssemblyProduct("Super Start")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Dylan Bickerstaff")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
